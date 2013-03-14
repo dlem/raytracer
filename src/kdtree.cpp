257b4737ce727248e6c04e52992385f94eedf297
Daniel Lemmond <daniellemmond@gmail.com>
2013-03-14 01:29:08 -0400

    Implement simple KDTree.

---
+++ src/kdtree.cpp
@@ -0,0 +1,51 @@
+#include "kdtree.hpp"
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+void kdtree_test()
+{
+  auto r = [] { return (rand() % 100000) * 0.001; };
+  const int num = 100;
+
+  vector<KDNode *> nodes;
+  for(int i = 0; i < num; i++)
+  {
+    KDNode *node = new KDNode;
+    node->pt = Point3D(r(), r(), r());
+    nodes.push_back(node);
+  }
+
+  KDTree<KDNode> tree(nodes.begin(), nodes.end());
+
+  for(int i = 0; i < 100; i++)
+  {
+    if(rand() % 2 == 0)
+    {
+      const Point3D pt = Point3D(r(), r(), r());
+      assert(!tree.find(pt));
+    }
+    else
+    {
+      const int idx = rand() % num;
+      const KDNode *node = nodes[idx];
+      const Point3D &pt = node->pt;
+      const KDNode *found = tree.find(pt);
+      assert(found);
+      assert(tree.find(pt) == node);
+    }
+  }
+
+  for(int i = 0; i < 100; i++)
+  {
+    const int idx = rand() % num;
+    const KDNode *node = nodes[idx];
+    const Point3D &pt = node->pt;
+    Point3D jittered = pt;
+    jittered[rand() % 3] += -0.00001 + rand() % 1000 * 0.00000002;
+    assert(tree.find_nn(jittered) == node);
+  }
+
+  cerr << "All tests passed." << endl;
+}
---
+++ src/kdtree.hpp
@@ -0,0 +1,142 @@
+/**
+ * Daniel Lemmond
+ * dlemmond
+ * 20302247
+**/
+
+#ifndef __KD_TREE_HPP__
+#define __KD_TREE_HPP__
+
+#include "algebra.hpp"
+#include <queue>
+
+struct KDNode
+{
+  KDNode() : lchild(0), rchild(0) {}
+  Point3D pt;
+  KDNode *lchild;
+  KDNode *rchild;
+};
+
+template<typename TNode>
+class KDTree
+{
+private:
+  struct PQNode
+  {
+    PQNode(const Point3D &pt, KDNode *node)
+      : node(node)
+      , dist((pt - node->pt).length())
+    {}
+    double dist;
+    KDNode *node;
+    bool operator<(const PQNode &other) const
+    {
+      return dist < other.dist;
+    }
+  };
+
+  typedef std::priority_queue<PQNode> TPQueue;
+
+public:
+
+  template<typename TIt>
+  KDTree(TIt start, TIt end)
+  {
+    m_root = build(start, end, 0);
+  }
+
+  TNode *find(const Point3D &pt)
+  {
+    return find(pt, m_root, 0);
+  }
+
+  TNode *find_nn(const Point3D &pt)
+  {
+    TPQueue q;
+    find_nnn(pt, 1, m_root, q, 0);
+    return q.top().node;
+  }
+
+  TNode *find_nnn(const Point3D &pt, int n, std::vector<TNode *> &ns)
+  {
+    TPQueue nns;
+    find_nnn(pt, n, nns); 
+    while(!nns.empty())
+    {
+      ns.push(nns.top().node);
+      nns.pop();
+    }
+  }
+
+private:
+
+  static KDNode *find(const Point3D &pt, KDNode *node, int depth)
+  {
+    const int split = depth % 3;
+
+    if(!node)
+      return 0;
+    const Point3D &npt = node->pt;
+    if(npt[0] == pt[0] && npt[1] == pt[1] && npt[2] == pt[2])
+      return node;
+    if(pt[split] < npt[split])
+      return find(pt, node->lchild, depth + 1);
+    return find(pt, node->rchild, depth + 1);
+  }
+
+  static void find_nnn(const Point3D &pt, int n, KDNode *node, TPQueue &nns, int depth)
+  {
+    const int split = depth % 3;
+
+    if(!node)
+      return;
+    
+    KDNode *tree1, *tree2;
+    if(pt[split] < node->pt[split])
+    {
+      tree1 = node->lchild;
+      tree2 = node->rchild;
+    }
+    else
+    {
+      tree1 = node->rchild;
+      tree2 = node->lchild;
+    }
+
+    find_nnn(pt, n, tree1, nns, depth + 1);
+
+    const double mindist = abs(pt[split] - node->pt[split]);
+    if(nns.size() < n || mindist < nns.top().dist)
+    {
+      nns.push(PQNode(pt, node));
+      if(nns.size() > n)
+	nns.pop();
+      find_nnn(pt, n, tree2, nns, depth + 1);
+    }
+  }
+
+  template<typename TIt>
+  static KDNode *build(TIt start, TIt end, int depth)
+  {
+    const int split = depth % 3;
+
+    if(start == end)
+      return 0;
+
+    std::sort(start, end, [split](const KDNode *c1, const KDNode *c2)
+	{ return c1->pt[split] < c2->pt[split]; });
+
+    auto median = start + (end - start) / 2;
+    KDNode *node = *median;
+    node->lchild = build(start, median, depth + 1);
+    node->rchild = build(median + 1, end, depth + 1);
+    return node;
+  }
+
+  TNode *m_root;
+};
+
+void kdtree_test();
+
+#endif
